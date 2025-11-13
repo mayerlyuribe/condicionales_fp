18aa6b7c9463dc1915b05f8505da1a4ec83322e1
mayerlyuribe <mayerlyuribe@colegioguanenta.edu.co>
2025-11-12 23:36:27 -0500

    finish 😭

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,15 +1,15 @@
 numero = int(input("ingrese un número entre 0 y 15: "))
 
-resultados = []
+resultados_bin = []
 temporal = numero
 
 while (True):
     if temporal > 0 and temporal <= 15:
         mod = temporal % 2
-        resultados.append(mod)
+        resultados_bin.append(mod)
         temporal //= 2
     elif temporal == 0:
-        resultados.append(0)
+        resultados_bin.append(0)
         break;
     else:
         print("número inválido")
@@ -17,8 +17,24 @@ while (True):
 
 num_invertido = ""
 
-for i in range(0, len(resultados)):
+for i in range(0, len(resultados_bin)):
     cont = -i-1
-    num_invertido += str(resultados[cont])
+    num_invertido += str(resultados_bin[cont])
 
 print("número en binario:", num_invertido)
+
+#---------------------------------------------
+
+hexa_dicc = {
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F"
+}
+
+if numero >=0 and numero <=9:
+    print("número en hexadecimal:", numero)
+elif numero >=10 and numero <=15:
+    print("número en hexadecimal:", hexa_dicc[numero])
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,33 @@
-# tareas
 
-# ponderaciones
+diccionario_tareas = {}
 
-# nota obtenida
+for i in range(1, 3):
+    tarea = input("ingrese el nombre de la tarea " + str(i) + ": ")
+    ponderacion = float(input("ingrese la ponderación de la tarea " + tarea + " (%): "))
+    nota = float(input("ingrese la nota obtenida en la tarea " + tarea + "(0.0 - 5.0): "))
+    diccionario_tareas[tarea] = ponderacion, nota
+    print("-------------------------")
+
+nota_final = 0
+ponderacion_total = 0
+
+for clave in diccionario_tareas:
+    ponderacion, nota = diccionario_tareas[clave]
+    print("tarea:", clave)
+    print("ponderación:", ponderacion, "%")
+    print("nota obtenida:", nota,"\n")
+    nota_final += (ponderacion * nota) / 100
+    ponderacion_total += ponderacion
+
+if nota_final >= 4.5 and nota_final <= 5.0:
+    desempeño = "excelente"
+elif nota_final >= 3.0 and nota_final < 4.5:
+    desempeño = "bueno"
+elif nota_final >= 2.5 and nota_final < 3.0:
+    desempeño = "regular"
+else:
+    desempeño = "deficiente"
+
+print("nota final del curso:", nota_final)
+print("ponderación total evaluada:", ponderacion_total, "%")
+print("desempeño final:", desempeño)
